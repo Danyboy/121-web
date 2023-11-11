c8ae9357035c4313a53780abbe97e03f22323645
Danil Mikhailov <d.mikhailov@semrush.com>
2023-11-11 20:17:54 +0100

    Add fetch messages

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-messageId = 5;
-
-function sendMessage() {
-  messageId++;
-
-  var messageText = $("#current_message_text").val();
-  var newMessage =
-    `<div class="message" speech-bubble pright acenter message-color id=`+ messageId +`>
-    <div class="title">Dany</div>
-    <p>` + messageText + `</p>
-  </div>`;
-  $("#current_message_text").val('')
-
-  $("#messages").append($(newMessage));
-  
-  document.getElementById(messageId).scrollIntoView();
-}
-
 document.onkeydown=function(event){
   if(event.key == 'Enter'){
     sendMessage();
   }
+}
+
+messageId = 5;
+
+fetchMessage();
+
+function fetchMessage(){
+  $.get("./chats/stas_XFRTS4FT/start_01.json", function (data, status) {
+    if (data.length) {
+      generateMessageList(JSON.parse(data));
+    }
+  });
+}
+
+function generateMessageList(messages) {
+  for (var i = 0; i < messages.length; i++) {
+    addNewMessage(messages[i].text, messages[i].author)
+  }
+}
+
+function sendMessage() {
+  messageId++;
+  authorName = "Dany"
+
+  var messageText = $("#current_message_text").val();
+  addNewMessage(messageText, authorName)
+  $("#current_message_text").val('')
+  
+  document.getElementById(messageId).scrollIntoView();
+}
+
+function addNewMessage(messageText, authorName) {
+  var newMessage =
+    `<div class="message" speech-bubble pright acenter message-color id=`+ messageId +`>
+    <div class="title">` + authorName + `</div>
+    <p>` + messageText + `</p>
+  </div>`;
+
+  $("#messages").append($(newMessage));
+
+}
+
+function myHistoryTimeout() {
+  myTimer = 1000 * 5;
+  setTimeout(function () {
+    getHistory();
+  }, myTimer);
 }
 
