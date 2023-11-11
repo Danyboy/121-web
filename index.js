ffc2e5a7469c6ff0757aa626c5105700357e86f5
Danil Mikhailov <d.mikhailov@semrush.com>
2023-11-11 19:52:14 +0100

    Add send, auto scroll

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+messageId = 5;
+
 function sendMessage() {
+  messageId++;
+
   var messageText = $("#current_message_text").val();
   var newMessage =
-    `<div class="message" speech-bubble pright acenter message-color>
+    `<div class="message" speech-bubble pright acenter message-color id=`+ messageId +`>
     <div class="title">Dany</div>
     <p>` + messageText + `</p>
   </div>`;
   $("#current_message_text").val('')
 
   $("#messages").append($(newMessage));
+  
+  document.getElementById(messageId).scrollIntoView();
 }
 
 document.onkeydown=function(event){
