b5bcc0e06861243e37a284ffb561a9479e55af43
Danil Mikhailov <d.mikhailov@semrush.com>
2023-11-12 10:44:49 +0100

    Fix send 2

--- app/send.php
+++ app/send.php
@@ -2,28 +2,34 @@
 
 $jsonData = file_get_contents('php://input');
 
-if (!empty($jsonData)) {
+saveMessage();
 
-  $currentChat = empty($jsonData['current_chat']);
-  if(!$currentChat){
-    $chatId = "stas_XFRTS4FT/start_01.json";
+function saveMessage()
+{
+  if (!empty($jsonData)) {
+
+    $currentChat = $jsonData['current_chat'];
+    if (!empty($currentChat)) {
+      $chatId = "stas_XFRTS4FT/start_01.json";
+    } else {
+      var_dump(http_response_code(404));
+      error_log("No such chat");
+      return;
+    }
+
+    $chatFile = "./chats/" . $chatId;
+
+    $data = json_decode($jsonData, true);
+
+    $inplement = file_get_contents($chatFile);
+    $tempArray = json_decode($inplement, true);
+    array_push($tempArray['messages'], $data);
+    $jsonData = json_encode($tempArray, JSON_UNESCAPED_UNICODE);
+    file_put_contents($chatFile, $jsonData);
+
+    var_dump(http_response_code(200));
   } else {
-    var_dump(http_response_code(404));
-    error_log("No such chat");
+    error_log("You give me empty query");
   }
 
-  $chatFile = "./chats/" . $chatId;
-
-  $data = json_decode($jsonData, true);
-
-  $inplement = file_get_contents($chatFile);
-  $tempArray = json_decode($inplement, true);
-  array_push($tempArray['messages'], $data);
-  $jsonData = json_encode($tempArray, JSON_UNESCAPED_UNICODE);
-  file_put_contents($chatFile, $jsonData);
-
-  var_dump(http_response_code(200));
-} else {
-  error_log("You give me empty query");
 }
-
